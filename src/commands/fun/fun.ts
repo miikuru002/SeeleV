f4e4f7b5c61f5a558a9e272897683c4e142fdf25
Jamutaq Ortega Vélez <jamutaqortegavelez@icloud.com>
2022-05-06 11:35:05 -0500

    mejora del comando /fun anime_info

--- src/commands/fun/fun.ts
+++ src/commands/fun/fun.ts
@@ -3,7 +3,8 @@ import {
 	MessageActionRow,
 	MessageButton,
 	MessageEmbed,
-	MessageAttachment
+	MessageAttachment,
+	TextChannel,
 } from "discord.js";
 import { embed_color } from "../../config";
 import { getInfoFromName } from "mal-scraper";
@@ -87,25 +88,25 @@ export default new Command({
 			case "8ball": {
 				const prg = args.getString("pregunta", true);
 				const rpts = [
-					"Sip", 
-					"Nop", 
-					"Tal vez...", 
-					"No lo sé...", 
-					"Quizás en un universo paralelo", 
-					"Quién sabe...", 
-					"Si bueno, quién tiene hambre okno", 
+					"Sip",
+					"Nop",
+					"Tal vez...",
+					"No lo sé...",
+					"Quizás en un universo paralelo",
+					"Quién sabe...",
+					"Si bueno, quién tiene hambre okno",
 					"Definitivemente",
 					"En efecto",
 					"No nya",
 					"Sí nya",
 					"aea",
 					"Solo sé que nada se",
-					"Sí, digo.. ¿que?"
-				]; 
-				
+					"Sí, digo.. ¿que?",
+				];
+
 				const random = rpts[Math.floor(Math.random() * rpts.length)];
-  
-				const embed8 = new MessageEmbed() 
+
+				const embed8 = new MessageEmbed()
 					.setTitle(":8ball: Pregunta 8ball:")
 					.setColor(embed_color)
 					.addField("Tu pregunta:", prg)
@@ -117,7 +118,7 @@ export default new Command({
 			}
 
 			case "anime_info": {
-				const name = args.getString("nombre", true); 
+				const name = args.getString("nombre", true);
 				const row = new MessageActionRow();
 				const ani_embed = new MessageEmbed();
 
@@ -127,74 +128,78 @@ export default new Command({
 					const data = await getInfoFromName(name);
 
 					if (data) {
-						ani_embed
-							.setTitle(":cherry_blossom: Anime info:")
-							.setImage(`${data.picture}`)
-							.setDescription(
-								`Información para: \`${name}\`\n\n**Sinopsis:**\n${
-									(await translate(`${data.synopsis}`, "es")) || "No especificado"
-								}`
-							)
-							.addField(
-								"**Título(s):**",
-								`__*Inglés:*__ ${data.englishTitle || "-"}\n__*Original:*__ ${
-									data.title
-								}`,
-								true
-							)
-							.addField(
-								"**Tipo y estado:**",
-								`${data.type || "No especificado"}, ${data.status || "No especificado"}`,
-								true
-							)
-							.addField(
-								"**Otros nombres:**",
-								`${data.synonyms.join(", ") || "No especificado"}`,
-								true
-							)
-							.addField(
-								"**Géneros:**",
-								`${data.genres?.join(", ") || "No especificado"}`,
-								true
-							)
-							.addField(
-								"**Episodios:**",
-								`${data.episodes || "No especificado"}`,
-								true
-							)
-							.addField(
-								"**Intervalo de emisión:**",
-								`${data.aired || "No especificado"}`,
-								true
-							)
-							.addField(
-								"**Popularidad:**",
-								`${data.popularity || "No especificado"}`,
-								true
-							)
-							.addField(
-								"**Puntuación:**",
-								`${data.score + " de 10" || "No especificado"}`,
-								true
-							)
-							.addField(
-								"**Clasificación de edad:**",
-								`${data.rating || "No especificado"}`,
-								true
-							)
-							.setColor(embed_color);
+						const chan = interaction.channel as TextChannel;
+						//si es el anime es +18
+						if (
+							(data.rating === "Rx - Hentai" ||
+								data.rating === "R+ - Mild Nudity") &&
+							!chan.nsfw
+						) {
+							return await interaction.editReply({
+								content:
+									"**:underage: | La información de este anime solo puede mostrarse en canales NSFW**",
+							});
 
-						row.addComponents(
-							new MessageButton()
-								.setURL(data.url)
-								.setLabel("Más información")
-								.setStyle("LINK")
-						);
+						} else {
+							ani_embed
+								.setTitle(":cherry_blossom: Anime info:")
+								.setImage(`${data.picture}`)
+								.setDescription(
+									`**Sinopsis:**\n${
+										(await translate(`${data.synopsis}`, "es")) ||
+										"No especificado"
+									}`
+								)
+								.addField(
+									"**Título(s):**",
+									`__*Inglés:*__ ${data.englishTitle || "-"}\n__*Original:*__ ${
+										data.title
+									}`,
+									true
+								)
+								.addField(
+									"**Tipo y estado:**",
+									`${data.type || "No especificado"} (${data.episodes || "-"} episodio/s), ${
+										(await translate(`${data.status}`, "es")) ||
+										"No especificado"
+									}`,
+									true
+								)
+								.addField(
+									"**Otros nombres:**",
+									`${data.synonyms.join(", ") || "No especificado"}`,
+									true
+								)
+								.addField(
+									"**Géneros:**",
+									`${data.genres?.join(", ") || "No especificado"}`,
+									true
+								)
+								.addField(
+									"**Intervalo de emisión:**",
+									`${data.aired || "No especificado"}`,
+									true
+								)
+								.addField(
+									"**Clasificación de edad:**",
+									`\`${data.rating || "No especificado"}\``,
+									true
+								)
+								.setColor(embed_color);
 
-						return await interaction.editReply({
-							embeds: [ani_embed],
-							components: [row],
-						});
+							row.addComponents(
+								new MessageButton()
+									.setURL(data.url)
+									.setLabel("Más información")
+									.setStyle("LINK")
+							);
+
+							return await interaction.editReply({
+								content: `Mostrando información para: \`${name}\`...`,
+								embeds: [ani_embed],
+								components: [row],
+							});
+						}
 					}
 
 					ani_embed
@@ -202,7 +207,7 @@ export default new Command({
 						.setDescription("No pude encontrar ese anime...")
 						.setColor(embed_color);
 					return await interaction.editReply({ embeds: [ani_embed] });
-
+					
 				} catch (err) {
 					ani_embed
 						.setTitle(":cherry_blossom: Anime info:")
@@ -218,9 +223,11 @@ export default new Command({
 
 				const dado_embed = new MessageEmbed()
 					.setTitle(":game_die: Tiro de dado:")
-					.setDescription(`**${interaction.user.username}**, obtuviste un: \`${num}\`!`)
+					.setDescription(
+						`**${interaction.user.username}**, obtuviste un: \`${num}\`!`
+					)
 					.setColor(embed_color);
-				
+
 				return await interaction.reply({ embeds: [dado_embed] });
 			}
 
@@ -229,8 +236,9 @@ export default new Command({
 
 				//si el mensaje contiene un @everyone/here o menciona a un rol
 				if (msg.match(/@(everyone|here)/gi) || msg.match(/<@&(\d{17,19})>/gi)) {
-					return await interaction.reply({ 
-						content: "**:no_entry_sign: | El mensaje no puede tener un everyone/here o mencionar algún rol**" 
+					return await interaction.reply({
+						content:
+							"**:no_entry_sign: | El mensaje no puede tener un everyone/here o mencionar algún rol**",
 					});
 				}
 
@@ -238,15 +246,16 @@ export default new Command({
 				await interaction.channel?.send({ content: msg });
 
 				//envía un mensaje efímero de confirmación (se debe responder al comando, sino sale error)
-				return await interaction.reply({ 
-					content: "Mensaje enviado (｡•̀ᴗ-)✧", ephemeral: true 
+				return await interaction.reply({
+					content: "Mensaje enviado (｡•̀ᴗ-)✧",
+					ephemeral: true,
 				});
 			}
 
 			case "say_sus": {
 				const user = args.getUser("usuario", true);
 				const msg = args.getString("mensaje", true);
-				const member = 	interaction.guild?.members.cache.get(user.id);
+				const member = interaction.guild?.members.cache.get(user.id);
 
 				if (member) {
 					//bot "pensando"
@@ -259,12 +268,14 @@ export default new Command({
 					const attach = new MessageAttachment(img);
 
 					//envia la imagen
-					return await interaction.editReply({ 
-						files: [{ attachment: attach.attachment, name: "say_sus.png" }], 
+					return await interaction.editReply({
+						files: [{ attachment: attach.attachment, name: "say_sus.png" }],
 					});
 				}
 
-				return await interaction.reply({ content: "Este usuario no existe..."});
+				return await interaction.reply({
+					content: "Este usuario no existe...",
+				});
 			}
 
 			default:
